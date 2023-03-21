cbc51dc6a23f36b91bf618f6a12e26c8b1efb050
handiism <mail.handira@gmail.com>
2023-03-21 10:07:29 +0700

    fix: logic,f* style

--- lib/detail.dart
+++ lib/detail.dart
@@ -35,12 +35,48 @@ class _DetailState extends State<Detail> {
             children: [
               Image.network(buku.imageLink),
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
+                padding: const EdgeInsets.all(7.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Judul\t\t\t\t\t: ${buku.title}"),
-                    Text("Judul\t\t\t\t\t: ${buku.author}"),
-                    Text("Judul\t\t\t\t\t: ${buku.author}"),
+                    Text(
+                      "Judul          : ${buku.title}",
+                    ),
+                    Text(
+                      "Penulis        : ${buku.author}",
+                    ),
+                    Text(
+                      "Bahasa         : ${buku.language}",
+                    ),
+                    Text(
+                      "Negara         : ${buku.country}",
+                    ),
+                    Text(
+                      "Jumlah Halaman : ${buku.pages}",
+                    ),
+                    Text(
+                      "Tahun Terbit   : ${buku.year}",
+                    ),
+                    Text(
+                      "Status         : ${buku.isAvailable ? "Tersedia" : "Tidak Tersedia"}",
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          if (buku.isAvailable) {
+                            listBuku[widget.index].isAvailable = false;
+                          }
+                        });
+                      },
+                      style: ButtonStyle(
+                          backgroundColor: buku.isAvailable
+                              ? MaterialStateProperty.all(Colors.blue)
+                              : MaterialStateProperty.all(Colors.grey)),
+                      child: const Text(
+                        "Pinjam Buku",
+                      ),
+                    )
                   ],
                 ),
               )
